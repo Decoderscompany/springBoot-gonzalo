04fa0340f68306b6bacb101beffcbed518ca1463
Decoderscompany <111249717+Decoderscompany@users.noreply.github.com>
2022-08-17 00:08:29 -0500

    ok, primer servicio "/hello"

--- src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
@@ -2,12 +2,25 @@ package com.UdeA.Ciclo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+
+import java.util.Arrays;
+
+@RestController
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Ciclo3Application {
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hola Ciclo3 .... Saldremos vivos de esto!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
+
 	}
 
 }
