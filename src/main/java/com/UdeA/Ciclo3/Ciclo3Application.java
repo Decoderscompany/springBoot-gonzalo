d6f1c433d1eada5f95cc7ed899520df0a07ae805
Decoderscompany <111249717+Decoderscompany@users.noreply.github.com>
2022-08-17 12:46:25 -0500

    Segundo Commit prueba servicio Test , con postgresql

--- src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
@@ -1,13 +1,12 @@
 package com.UdeA.Ciclo3;
 
+import com.UdeA.Ciclo3.modelos.Empresa;
+import org.hibernate.annotations.GeneratorType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
-import java.util.Arrays;
 
 @RestController
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
@@ -18,9 +17,15 @@ public class Ciclo3Application {
 		return "Hola Ciclo3 .... Saldremos vivos de esto!";
 	}
 
+	@GetMapping("/test")
+	public String test(){
+		Empresa emp = new Empresa("SOLAR SAS", "Calle La geta",  "31212312233", "800123555");
+		emp.setNombre("SOLAR Punto NET");
+		return emp.getNombre();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
-
 	}
 
 }
